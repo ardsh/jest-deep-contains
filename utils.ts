6740e4a91d8c185656c452ad60fcc1467def0756
ardsh <23664687+ardsh@users.noreply.github.com>
2021-11-22 10:40:46 +0100

    Fixed test utils

--- utils.ts
+++ utils.ts
@@ -20,23 +20,30 @@ export function deepObjContaining(obj: any): any {
 }
 
 
-export interface TesterConfig<TData, TSetup> {
-    run: (data: TData) => Promise<any>,
-    //TODO: Add partial inference when available
+export interface TesterConfig<TData, TSetup={}, TBefore={}> {
+    run: (data: TData, context: TSetup & TBefore) => any,
+    beforeEach?: (data: TData, context: TSetup) => Promise<TBefore>,
     setup?: (tests: TData[]) => Promise<TSetup> | TSetup,
-    teardown?: (data: TSetup, tests: TData[]) => any,
+    teardown?: (ctx: TSetup, tests: TData[]) => any,
 }
 
 interface Test<TData> {
-    run?: (data: TData) => Promise<any>,
+    run?: (data: TData, ctx?: any) => any,
     data: TData,
     text: string,
 }
 
-export const makeTester = <TData extends object, TSetup=any>(config?: TesterConfig<TData, TSetup>) => {
-    const tests: Test<TData>[] = [];
+export const makeTester = <TSetup, TPData extends object>(conf?: Omit<TesterConfig<any, TSetup>, "run">) => <TData extends object=TPData, TBefore={}>(config?: Partial<Pick<TesterConfig<TData, TSetup, TBefore>, "run" | "beforeEach">>) => {
+    let tests: Test<TData>[] = [];
+    config = {
+        ...conf,
+        ...config,
+    } as any;
     const self = {
-        addTest: (text: string, data: TData, run?: (data: TData) => Promise<any>) => {
+        clearTests: (match?: string | RegExp) => {
+            tests = !match ? [] : tests.filter(test => !test.text.match(match));
+        },
+        addTest: (text: string, data: TData, run?: TesterConfig<TData, TSetup, TBefore>["run"]) => {
             const test = {
                 text,
                 data,
@@ -45,22 +52,37 @@ export const makeTester = <TData extends object, TSetup=any>(config?: TesterConf
             tests.push(test);
             return {
                 ...self,
+                focus: () => {
+                    const index = tests.indexOf(test);
+                    tests.splice(0, index);
+                    tests.splice(1, 1000);
+                },
                 skip: () => {
                     tests.splice(tests.indexOf(test), 1);
                     return self;
                 }
             }
         },
-        runTests: () => {
+        runTests: <TPSetup=TSetup, TPBefore=TBefore>(overconf?: Partial<TesterConfig<TData, TPSetup, TPBefore>>) => {
             let setup = Promise.resolve(null as any);
-            if (config?.setup) {
-                beforeAll(() => setup = Promise.resolve(config.setup!(tests.map(t => t.data))));
+            const setupFunc = overconf?.setup || conf?.setup;
+            if (setupFunc) {
+                beforeAll(() => setup = Promise.resolve(setupFunc(tests.map(t => t.data))));
             }
-            if (config?.teardown) {
-                afterAll(() => setup.then(s => config.teardown!(s, tests.map(t => t.data))));
+            const teardownFunc = overconf?.teardown || conf?.teardown;
+            if (teardownFunc) {
+                afterAll(() => setup.then(s => teardownFunc(s, tests.map(t => t.data))));
             }
             tests.forEach(test => it(test.text, async () => {
-                return (test.run || config?.run)!(test.data);
+                const beforeSingle = overconf?.beforeEach || config?.beforeEach || conf?.beforeEach;
+                let ctx = await beforeSingle?.(test.data, await setup);
+                const runFunc = test.run || overconf?.run || config?.run;
+                if (typeof runFunc !== 'function') {
+                    expect(test).toEqual(expect.objectContaining({
+                        run: expect.anything(),
+                    }));
+                }
+                return runFunc!(test.data, { ...await setup, ...ctx });
             }));
         }
     }
